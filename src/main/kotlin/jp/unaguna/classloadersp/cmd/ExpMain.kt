1218f06fed971614339f279e45ed2d0d40c7df86
K-izumi <k.izumi.ysk@gmail.com>
2025-08-12 21:11:27 +0900

    🐛 ignore the default class loader

--- src/main/kotlin/jp/unaguna/classloadersp/cmd/ExpMain.kt
+++ src/main/kotlin/jp/unaguna/classloadersp/cmd/ExpMain.kt
@@ -6,7 +6,7 @@ import java.net.URLClassLoader
 
 fun main() {
     val classpath = classpathSpecToURLArray("C:\\programs\\class-loader-cmd-sandbox\\one.jar;C:\\programs\\class-loader-cmd-sandbox\\two.jar")
-    val classLoader = URLClassLoader(classpath)
+    val classLoader = URLClassLoader(classpath, null)
     val defProvider = SubclassDefinitionProvider(
         classLoader,
         superClass = classLoader.loadClass("com.A"),
