e0dc95e8c3360169045d71a921c0e33e2d6707df
K-izumi <k.izumi.ysk@gmail.com>
2025-08-14 15:40:41 +0900

    🚧 remove ClassPathScanningCandidateComponentProvider

--- src/main/kotlin/jp/unaguna/classloader/sp/SpringClasspathScanner.kt
+++ src/main/kotlin/jp/unaguna/classloader/sp/SpringClasspathScanner.kt
@@ -2,23 +2,22 @@ package jp.unaguna.classloader.sp
 
 import jp.unaguna.classloader.core.ClasspathScannerResettable
 import jp.unaguna.classloader.core.ScannedElement
-import jp.unaguna.classloader.sp.tree.ExtendClassTree
-import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider
-import org.springframework.core.io.DefaultResourceLoader
 import org.springframework.core.io.Resource
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.io.support.ResourcePatternResolver
 import org.springframework.core.type.ClassMetadata
 import org.springframework.core.type.classreading.CachingMetadataReaderFactory
+import org.springframework.core.type.classreading.MetadataReader
 import org.springframework.core.type.filter.AssignableTypeFilter
+import org.springframework.core.type.filter.TypeFilter
 import java.net.URL
 
 class SpringClasspathScanner(
-    private val classLoader: ClassLoader,
+    classLoader: ClassLoader,
 ): ClasspathScannerResettable<ClassFileMetadata, SpringClasspathScannerElement> {
     private var basePackage: String? = null
     private var classExtensionTree: Boolean = false
+    private val includeFilters: MutableList<TypeFilter> = mutableListOf()
 
     private val resolver = PathMatchingResourcePatternResolver(classLoader)
 
@@ -27,14 +26,20 @@ class SpringClasspathScanner(
         return SpringClasspathScannerIterator(
             resolver.getResources(packageSearchPath).toList(),
             classExtensionTree = this.classExtensionTree,
-            classLoader = this.classLoader,
             resolver = this.resolver,
+            includeFilters = this.includeFilters,
         )
+    }
+    private fun addIncludeFilter(typeFilter: TypeFilter) {
+        includeFilters.add(typeFilter)
+    }
+
+    private fun resetIncludeFilter() {
+        includeFilters.clear()
     }
 
     override fun subclassOf(cls: Class<*>) {
-        TODO()
-//        scanner.addIncludeFilter(AssignableTypeFilter(cls))
+        this.addIncludeFilter(AssignableTypeFilter(cls))
     }
 
     override fun inPackage(basePackage: String) {
@@ -46,15 +51,15 @@ class SpringClasspathScanner(
     }
 
     override fun clearConditions() {
-        TODO()
-        // scanner.resetFilters(false)
-        // basePackage = null
-        // classExtensionTree = false
+         this.resetIncludeFilter()
+         basePackage = null
+         classExtensionTree = false
     }
 }
 
 data class ClassFileMetadata(
     val resource: Resource,
+    val metadataReader: MetadataReader,
     val classMetadata: ClassMetadata,
 )
 
@@ -62,7 +67,7 @@ private class SpringClasspathScannerIterator(
     scanned: Iterable<Resource>,
     resolver: ResourcePatternResolver,
     classExtensionTree: Boolean,
-    classLoader: ClassLoader,
+    private val includeFilters: List<TypeFilter>,
 ): Iterator<SpringClasspathScannerElement> {
     val metadataReaderFactory = CachingMetadataReaderFactory(resolver)
     val innerIterator = if (classExtensionTree) {
@@ -72,18 +77,41 @@ private class SpringClasspathScannerIterator(
         .map { Pair(it, 0) }
         .iterator()
 
+    var nextElement: SpringClasspathScannerElement? = null
+
+    init {
+        calcNext()
+    }
+
+    /**
+     * Calc the next element and contain it into [nextElement]
+     */
+    private fun calcNext() {
+        while (innerIterator.hasNext()) {
+            val (nextResource, depth) = innerIterator.next()
+            val metadataReader = metadataReaderFactory.getMetadataReader(nextResource)
+            val classMetadata = metadataReader.classMetadata
+
+            if (includeFilters.all { it.match(metadataReader, metadataReaderFactory) }) {
+                nextElement = SpringClasspathScannerElement(
+                    ClassFileMetadata(nextResource, metadataReader, classMetadata),
+                    depth = depth,
+                )
+                return
+            }
+        }
+
+        nextElement = null
+    }
+
     override fun next(): SpringClasspathScannerElement {
-        val (nextResource, depth) = innerIterator.next()
-        val metadataReader = metadataReaderFactory.getMetadataReader(nextResource)
-        val metadata = metadataReader.classMetadata
-        return SpringClasspathScannerElement(
-            ClassFileMetadata(nextResource, metadata),
-            depth = depth,
-        )
+        val next = nextElement ?: throw NoSuchElementException()
+        calcNext()
+        return next
     }
 
     override fun hasNext(): Boolean {
-        return innerIterator.hasNext()
+        return nextElement != null
     }
 }
 
@@ -102,18 +130,5 @@ class SpringClasspathScannerElement(
 
     override fun toString(): String {
         return "${this.javaClass.simpleName}(${className}, depth = ${depth})"
-    }
-}
-
-private class CustomClassPathScanningCandidateComponentProvider(
-    classLoader: ClassLoader,
-): ClassPathScanningCandidateComponentProvider(false) {
-    init {
-        resourceLoader = DefaultResourceLoader(classLoader)
-    }
-
-    override fun isCandidateComponent(beanDefinition: AnnotatedBeanDefinition): Boolean {
-        // 元の実装だと抽象クラスやインターフェース (つまり Bean にできないもの) が対象外になってしまうので改造
-        return true
     }
 }
--- src/test/kotlin/jp/unaguna/classloader/sp/SpringClasspathScannerTest.kt
+++ src/test/kotlin/jp/unaguna/classloader/sp/SpringClasspathScannerTest.kt
@@ -6,17 +6,34 @@ import kotlin.test.assertEquals
 
 class SpringClasspathScannerTest {
     @Test
-    fun testScanByBaseClass() {
+    fun testScanAll() {
         val cpJarUrl = SpringClasspathScannerTest::class.java.getClassLoader().getResource("jar_for_test/sample.jar")
         val classLoader = URLClassLoader(arrayOf(cpJarUrl), null)
-        val scanner = SpringClasspathScanner(classLoader).apply {
-            subclassOf(classLoader.loadClass("java.lang.Object"))
-        }
+        val scanner = SpringClasspathScanner(classLoader)
 
         val result = scanner.scan().asSequence().toList()
 
         println(result)
 
         assertEquals(5, result.size)
+    }
+
+    @Test
+    fun testScanByBaseClass() {
+        val cpJarUrl = SpringClasspathScannerTest::class.java.getClassLoader().getResource("jar_for_test/sample.jar")
+        val classLoader = URLClassLoader(arrayOf(cpJarUrl), null)
+        val scanner = SpringClasspathScanner(classLoader).apply {
+            subclassOf(classLoader.loadClass("com.example.BaseClass"))
+        }
+
+        val result = scanner.scan().asSequence().toList()
+
+        println(result)
+
+        assertEquals(2, result.size)
+        assertEquals(
+            setOf("com.example.AbsSubClass", "com.example.BaseClass"),
+            result.map { it.className }.toSet(),
+        )
     }
 }
